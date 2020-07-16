400ede513e98f39cf01c55e8d7a8de99cd25efb2
Vrushank Patel <34762923+VrushankPatel@users.noreply.github.com>
2020-07-16 10:34:56 +0530

    added new files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var portfolio = [
-    "http://vapsquare.000webhostapp.com/Portfolio-1/index.html",    
-    "http://vapsquare.000webhostapp.com/Portfolio-2/index.html",
-    "http://vapsquare.000webhostapp.com/Portfolio-3/profile.html"
+    "https://vapsquare.000webhostapp.com/Portfolio-1/index.html",    
+    "https://vapsquare.000webhostapp.com/Portfolio-2/index.html",
+    "https://vapsquare.000webhostapp.com/Portfolio-3/profile.html"
 ]
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
