285d9010c23cd1c32b1dab12c25c7005000ebdd2
Vrushank Patel <34762923+VrushankPatel@users.noreply.github.com>
2020-08-09 16:39:28 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-var portfolio = [
-    "https://vrushankpatel.000webhostapp.com/"
-]
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-document.getElementById("mainframe").src = portfolio[getRandomInt(3)];
+document.getElementById("mainframe").src = "https://vrushankpatel.000webhostapp.com"
